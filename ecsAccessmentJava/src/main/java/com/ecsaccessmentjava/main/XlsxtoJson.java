d7b3351e110d5d4e832dec18f2c8c555b6b3373a
Arun Sreeram M <81244343+ArunSreec@users.noreply.github.com>
2022-09-05 09:02:43 +0530

    Add files via upload

---
+++ ecsAccessmentJava/src/main/java/com/ecsaccessmentjava/main/XlsxtoJson.java
@@ -0,0 +1,20 @@
+package com.ecsaccessmentjava.main;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+
+
+public class XlsxtoJson {
+
+	public void main(String[] args) throws IOException, SQLException{
+	
+		Details newDet = new Details();
+		newDet.allDetails();
+	 
+	
+	}
+
+	
+}
+
